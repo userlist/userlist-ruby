ebc91341b61380bb2ca4406cc174afa0169f043d
Jakob Skjerning <jakob@mentalized.net>
2020-03-16 14:14:37 +0100

    WIP: Demonstrate error when instantiating Client

--- spec/userlist/push/client_spec.rb
+++ spec/userlist/push/client_spec.rb
@@ -8,6 +8,22 @@ RSpec.describe Userlist::Push::Client do
   end
 
   describe '#initialize' do
+    context 'when configured with nothing' do
+      it 'gets push key from the environment' do
+        ENV['USERLIST_PUSH_KEY'] = 'test-push-key'
+        instance = described_class.new
+        ENV.delete('USERLIST_PUSH_KEY')
+        expect(instance.send(:token)).to eq('test-push-key')
+      end
+    end
+
+    context 'when configured with a Hash' do
+      it 'gets push key from the hash' do
+        instance = described_class.new(push_key: 'test-push-key')
+        expect(instance.send(:token)).to eq('test-push-key')
+      end
+    end
+
     context 'when the push_key is missing' do
       let(:config) do
         Userlist::Config.new(push_key: nil, push_endpoint: 'https://endpoint.test.local')
