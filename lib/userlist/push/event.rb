0f911a48ebdd296fa5a0583ef32a1511c551adb7
Benedikt Deicke <benedikt@benediktdeicke.com>
2020-08-14 10:08:18 +0200

    Adds support for company events

--- lib/userlist/push/event.rb
+++ lib/userlist/push/event.rb
@@ -4,9 +4,10 @@ module Userlist
       def initialize(attributes = {}, config = Userlist.config)
         raise Userlist::ArgumentError, 'Missing required attributes hash' unless attributes
         raise Userlist::ArgumentError, 'Missing required parameter :name' unless attributes[:name]
-        raise Userlist::ArgumentError, 'Missing required parameter :user' unless attributes[:user]
+        raise Userlist::ArgumentError, 'Missing required parameter :user or :company' unless attributes[:user] || attributes[:company]
 
-        attributes[:user] = Userlist::Push::User.from_payload(attributes[:user])
+        attributes[:user] &&= Userlist::Push::User.from_payload(attributes[:user], config)
+        attributes[:company] &&= Userlist::Push::Company.from_payload(attributes[:company], config)
         attributes[:occured_at] ||= Time.now
 
         super
